914e8f0edcdc51e9e19d714a8ea12a4f665c4457
meteoratlas <kellyaccnts@gmail.com>
2021-12-25 15:43:44 -0700

    finished type annotations & inference

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -30,3 +30,25 @@ let point: { x: number; y: number } = {
 const logNum: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// when to use type annotations
+// 1) when a function returns the 'any' type
+const json = '{"x":10, "y":20}';
+const coords: { x: number; y: number } = JSON.parse(json);
+console.log(coords);
+
+// 2) when we declare and initialize a var at different times
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let index = 0; index < words.length; index++) {
+  if (words[index] === "green") foundWord = true;
+}
+
+// 3) type cannot be reasonably inferred
+let numbers = [-10, -1, 12];
+let numAboveZero: boolean | number = false; // pipe to represent 'or' - number or boolean
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) numAboveZero = numbers[i];
+}
