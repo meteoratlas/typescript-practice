b60090219d11043d2dbd2b662443ce6645ddf967
meteoratlas <kellyaccnts@gmail.com>
2021-12-25 15:14:44 -0700

    basic vars

---
+++ features/annotations/variables.ts
@@ -0,0 +1,11 @@
+let apples: number = 5;
+
+let speed: string = "fast";
+
+let hasName: boolean = true;
+
+let nothing: null = null;
+
+let undef: undefined = undefined;
+
+let now: Date = new Date();
