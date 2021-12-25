2a8f116b7041ee22bb5d9134cfa857388e037371
meteoratlas <kellyaccnts@gmail.com>
2021-12-24 19:38:45 -0700

    use ts type template

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -3,5 +3,13 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/todos/1";
 axios_1["default"].get(url).then(function (res) {
-    console.log(res.data);
+  const todo = res.data;
+  const id = todo.ID;
+  const title = todo.Title;
+  const finished = todo.finished;
+
+  console.log(`
+    The todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}`);
 });
