5e439bb66deb83bc4c7016bd0f396d1d7304a9f9
meteoratlas <kellyaccnts@gmail.com>
2021-12-25 15:18:36 -0700

    object vars

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -9,3 +9,19 @@ let nothing: null = null;
 let undef: undefined = undefined;
 
 let now: Date = new Date();
+
+//
+let colors: string[] = ["red", "green", "blue"];
+
+let nyNymbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true];
+
+class Car {}
+
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
