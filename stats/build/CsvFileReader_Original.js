d81855cd30121e0b4b3bcf2e30b074b6d0961ba9
meteoratlas <kellyaccnts@gmail.com>
2022-05-18 21:15:43 -0600

    extract map row function

--- stats/build/CsvFileReader_Original.js
+++ stats/build/CsvFileReader_Original.js
@@ -20,17 +20,18 @@ var CsvFileReader = /** @class */ (function () {
             .map(function (row) {
             return row.split(",");
         })
-            .map(function (row) {
-            return [
-                (0, utils_1.dateStringToDate)(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                row[5],
-                row[6],
-            ];
-        });
+            .map(this.mapRow);
+    };
+    CsvFileReader.prototype.mapRow = function (row) {
+        return [
+            (0, utils_1.dateStringToDate)(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5],
+            row[6],
+        ];
     };
     return CsvFileReader;
 }());
