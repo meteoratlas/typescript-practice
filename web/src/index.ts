bf6341facb21c9aa9f8c699717f7dca07b98f431
meteoratlas <kellyaccnts@gmail.com>
2022-05-19 23:44:38 -0600

    getting user going

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hi there!');
+import { User } from "./models/User";
+
+console.log("Hi there!");
+
+const user = new User({ name: "bret", age: 20 });
+
+user.set({ name: "polly", age: 999 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
