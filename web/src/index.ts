53df37d1a5aa91a2f4600ab730aa08ef9e443b85
meteoratlas <kellyaccnts@gmail.com>
2022-05-20 20:35:16 -0600

    2nd refactor for user

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,21 +1,10 @@
 import { User } from "./models/User";
+import axios from "axios";
 
-console.log("Hi there!");
+const user = new User({ id: 1, name: "bret", age: 0 });
 
-const user = new User({ name: "bret", age: 20 });
-
-user.set({ name: "polly", age: 999 });
-
-user.on("change", () => {
-  console.log("change 1");
-});
-user.on("change", () => {
-  console.log("change 2");
-});
-user.on("save", () => {
-  console.log("save was triggered");
+user.on("change", function () {
+  console.log(user);
 });
 
-user.trigger("change");
-user.trigger("save");
-user.trigger("fake ");
+user.save();
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,35 +1,61 @@
-interface UserProps {
+import { AxiosResponse } from "axios";
+import { Attributes } from "./Attributes";
+import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
+
+export interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
 
-type Callback = () => void;
+const rootURL = "http://localhost:3000/users";
 
 export class User {
-  events: { [key: string]: Callback[] } = {};
-  constructor(private data: UserProps) {}
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootURL);
+  public attributes: Attributes<UserProps>;
 
-  get(propName: string): string | number {
-    return this.data[propName];
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
   }
 
   set(update: UserProps): void {
-    Object.assign(this.data, update);
+    this.attributes.set(update);
+    this.events.trigger("change");
   }
 
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
+  fetch(): void {
+    const id = this.attributes.get("id");
+    if (typeof id !== "number") {
+      throw new Error("cannot fetch without an id");
+    }
 
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) return;
-
-    handlers.forEach((callback) => {
-      callback();
+    this.sync.fetch(id).then((res: AxiosResponse): void => {
+      this.set(res.data);
     });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((res: AxiosResponse): void => {
+        this.trigger("save");
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
   }
 }
