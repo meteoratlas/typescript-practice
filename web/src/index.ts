3ae327e28dd37f697b07b8f027823b250b0cae80
meteoratlas <kellyaccnts@gmail.com>
2022-05-21 22:53:38 -0600

    done project (collectionview doesn't render, revist another time)

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,27 @@
-import { User } from "./models/User";
-import { UserForm } from "./views/UserForm";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
+import { UserEdit } from "./views/UserEdit";
+import { UserList } from "./views/UserList";
 
 const user = User.buildUser({ name: "Name!", age: 201 });
-const userForm = new UserForm(document.getElementById("root"), user);
-userForm.render();
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+users.on("change", () => {
+  const root = document.getElementById("root");
+  if (root) {
+    new UserList(root, users).render();
+    console.log("doot", users);
+  }
+});
+users.fetch();
+// const root = document.getElementById("root");
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+//   userEdit.render();
+// } else {
+//   throw new Error("No root element was found.");
+// }
--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,14 +1,12 @@
-import { User } from "../models/User";
+import { User, UserProps } from "../models/User";
+import { View } from "./View";
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel();
-  }
-
+export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void } {
     return {
       "click:.set-age": this.onSetAgeClick,
       "click:.set-name": this.onSetNameClick,
+      "click:.do-save:": this.onSaveClick,
     };
   }
 
@@ -18,44 +16,23 @@ export class UserForm {
 
   onSetNameClick = (): void => {
     const input = this.parent.querySelector("input");
-    const name = input.value;
-    this.model.set({ name });
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
+  };
+
+  onSaveClick = (): void => {
+    this.model.save();
   };
 
   template(): string {
     return `
         <div>
-        <h1>User Form</h1>
-        <div>User name: ${this.model.get("name")}</div>
-        <div>User age: ${this.model.get("age")}</div>
-        <input />
+        <input placeholder='${this.model.get("name")}' />
         <button class='set-name'>Update Name</button>
         <button class='set-age'>Set Random Age</button>
+        <button class='do-save'>Save</button>
         </div>`;
-  }
-
-  bindModel(): void {
-    this.model.on("change", () => {
-      this.render();
-    });
-  }
-
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(":");
-
-      fragment.querySelectorAll(selector).forEach((element) => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
-
-  render(): void {
-    this.parent.innerHTML = "";
-    const templateElement = document.createElement("template");
-    templateElement.innerHTML = this.template();
-    this.bindEvents(templateElement.content);
-    this.parent.append(templateElement.content);
   }
 }
